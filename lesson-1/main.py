0c0af30602fb5f9285a1035992fe573b9479c518
QuocTK <tranquocbn@gmail.com>
2025-02-08 09:34:09 +0700

    Add lesson 1 - Hello World

---
+++ lesson-1/main.py
@@ -0,0 +1,7 @@
+#Đây là comment 1 dòng. Dòng comment sẽ không được thực thi khi chạy
+'''
+    Đây là comment trong Python nhiều dòng 1
+    Đây là comment trong Python nhiều dòng 2
+    Đây là comment trong Python nhiều dòng 3
+'''
+print('Hello World!')
