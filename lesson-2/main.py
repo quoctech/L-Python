b59868011f11821f0c272fbeaed4fc8bc0a987bb
QuocTK <tranquocbn@gmail.com>
2025-02-08 09:50:07 +0700

    Add lesson 2 - Variable

---
+++ lesson-2/main.py
@@ -0,0 +1,35 @@
+
+# một số nguyên 5 được gán cho biến x
+# một chuỗi kí tự (string - str) L-Python được gán cho biến y
+x = 5
+y = "L-Python"
+print(x)
+print(y)
+
+#Casting
+str3 = str(3)    # str3 will be '3'
+int3 = int(3)    # int3 will be 3
+float3 = float(3)  # float3 will be 3.0 
+
+# Get the Type
+# Lấy kiểu dữ liệu của biến có thể dùng type() function
+print(type(str3))
+print(type(float3))
+
+#1 chuỗi có thể được khai báo bằng dấu "" hoặc ''
+singleQuotes = 'L-Python'
+doubleQuotes = "L-Python"
+print(singleQuotes, doubleQuotes)
+
+#Biến phân biệt hoa thường
+a = 10
+A = 20
+print(a, A)
+
+vTrue = True
+vFalse = False
+print(vTrue, vFalse)
+
+#Khai báo biến 1 dòng
+name, age, sex = 'L-Python', 18, True
+print(name, age, sex)
