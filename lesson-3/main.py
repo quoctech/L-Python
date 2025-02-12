d769be8939b40fc305c8a853a1b9c7aca0d7530f
QuocTK <tranquocbn@gmail.com>
2025-02-12 15:21:43 +0700

    Add lession 3 - number in Python

---
+++ lesson-3/main.py
@@ -0,0 +1,25 @@
+# Kiểu số trong Python
+# Có 3 loại: int, float, complex
+
+x = 1       # int
+y = 2.8     # float
+z = 1j      # complex
+
+#print(x, y, z)
+#print(type(x))
+#print(type(y))
+#print(type(z))
+
+# Type conversion
+# convert from int t float
+a = float(x)
+b = int(y)
+c = complex(z)
+
+# print(a)
+# print(b)
+# print(c)
+
+# Random number
+import random
+print(random.randrange(1, 10))
